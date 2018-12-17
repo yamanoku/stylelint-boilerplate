9e57529e3f22f7f278104a4e681ce9d5b05ae4dc
yamanoku <0910yama@gmail.com>
2018-12-17 18:55:07 +0900

    update gulp 3 => 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,43 @@
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*'],
-    replaceString: /\bgulp[\-.]/
+const gulp = require("gulp");
+const $ = require("gulp-load-plugins")({
+  pattern: ["gulp-*", "gulp.*"],
+  replaceString: /\bgulp[\-.]/
 });
-const stylelint = require('stylelint');
-const reporter = require('postcss-reporter');
-const stylelintConfig = require('@geekcojp/stylelint-config');
+const stylelint = require("stylelint");
+const reporter = require("postcss-reporter");
+const stylelintConfig = require("@geekcojp/stylelint-config");
 
-gulp.task('sass', ["lintStyles"], function() {
+gulp.task("sass", function() {
   return gulp
-    .src('sass/**/*.scss')
+    .src("sass/**/*.scss")
     .pipe($.plumber())
-    .pipe($.sass({style : 'expanded'}))
+    .pipe($.sass({ style: "expanded" }))
     .pipe($.csscomb())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest("dist/css"));
 });
 
-gulp.task("lintStyles", function() {
-  return gulp.src('sass/**/*.scss')
-    .pipe(
-    $.postcss([
-      stylelint(stylelintConfig),
-      reporter({
-        clearMessages: true
-      })
-    ])
+gulp.task("lint-styles", function() {
+  return gulp.src("sass/**/*.scss").pipe(
+    $.postcss(
+      [
+        stylelint(stylelintConfig),
+        reporter({
+		  clearReportedMessages: true
+        })
+      ],
+      {
+        syntax: require("postcss-scss")
+      }
+    )
   );
 });
 
-gulp.task('watch', function () {
-    gulp.watch(["sass/**/*.scss", "!sass/**/_*", "!sass/_**/*.scss"], ['sass']);
+gulp.task("watch", function() {
+  gulp.watch(["sass/**/*.scss"], gulp.task("sass"));
+  gulp.watch(["sass/**/*.scss"], gulp.task("lint-styles"));
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("sass", "lint-styles", "watch"))
+);
